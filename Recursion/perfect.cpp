d36ad21c54d956020e4794b7a87aaa4a0f56239e
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-23 20:00:39 +0530

    all_perfect

--- Recursion/perfect.cpp
+++ Recursion/perfect.cpp
@@ -15,7 +15,7 @@ void is_perfect(){
         cout << n << " is not a perfect number" << endl;
     }
 }
-int is_perfect2(int n){
+void is_perfect2(int n){
     int sum = 0;
     for(int i = 1; i <= n/2; i++){
         if(n % i == 0){
@@ -28,6 +28,24 @@ int is_perfect2(int n){
         cout << n << " is not a perfect number" << endl;
     }
 }
+int is_perfect3(){
+    int n;
+    cout<<"Enter num ";
+    cin>>n;
+    int sum = 0;
+    for(int i = 1; i <= n/2; i++){ 
+        if(n % i == 0){
+            sum += i;
+        }
+    }
+    if(sum == n){
+        cout << n << " is a perfect number" << endl;
+    } else {
+         cout << n << " is not a perfect number" << endl;
+    }
+    return 0;
+     
+}
 
 
 int main(){
@@ -37,6 +55,8 @@ int main(){
     cout<<"Enter your number : ";
     cin>>n;
     is_perfect2(n);
+    cout<<endl;
+    is_perfect3();
 
 
 
