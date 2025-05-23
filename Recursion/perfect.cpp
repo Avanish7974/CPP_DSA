b4eebdcdc9d893ac33034b338e42004ecc52fcd3
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-23 19:53:05 +0530

    num

--- Recursion/perfect.cpp
+++ Recursion/perfect.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void is_perfect(){
     int n = 6;
     int sum = 0;
-    for(int i = 1; i <= n/2; i++){
+    for(int i = 1; i <= n/2; i++){ 
         if(n % i == 0){
             sum += i;
         }
