e56a98a79b6d65ac523fbc7360660aa77b612f4d
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-24 23:16:51 +0530

    push

--- Recursion/perfect.cpp
+++ Recursion/perfect.cpp
@@ -50,6 +50,10 @@ int is_perfect3(){
 
 int main(){
     cout << "Hello World!" << endl;
+    // 1. Taking nothing and returning nothing
+    // 2. Taking nothing and returning something
+    // 3. Taking something and returning nothing
+    // 4. Taking something and returning something
 
     is_perfect();
     int n;
