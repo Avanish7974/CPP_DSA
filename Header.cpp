13fafb20fd7dca855f6b9d29eca6f86e2916c9c5
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-25 23:22:35 +0530

    ctrl

--- Header.cpp
+++ Header.cpp
@@ -4,6 +4,7 @@ int main(){
     cout << "Hello World!" << endl;
     cout<< "This is a header file example." << endl;
     cout << "Header files are used to include libraries in C++." << endl;
+    cout << "They contain function declarations and macros." << endl;
 
 
 
