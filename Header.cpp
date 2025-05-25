931d2fe0011be041055c42c26b79dd1e976da190
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-25 23:22:15 +0530

    commt

--- Header.cpp
+++ Header.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     cout << "Hello World!" << endl;
     cout<< "This is a header file example." << endl;
+    cout << "Header files are used to include libraries in C++." << endl;
 
 
 
