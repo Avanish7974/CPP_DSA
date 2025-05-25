a5b13c4469d404dee181fde7f1b987abe5935273
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-25 23:22:52 +0530

    ctrl

--- Header.cpp
+++ Header.cpp
@@ -5,6 +5,7 @@ int main(){
     cout<< "This is a header file example." << endl;
     cout << "Header files are used to include libraries in C++." << endl;
     cout << "They contain function declarations and macros." << endl;
+    cout << "For example, <iostream> is a header file that allows us to use input and output streams." << endl;
 
 
 
