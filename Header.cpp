94f03af0faf0cb5a9de0be79b20f18488561f7c4
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-25 23:24:00 +0530

    clear

--- Header.cpp
+++ Header.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello World!" << endl;
-    cout<< "This is a header file example." << endl;
-    cout << "Header files are used to include libraries in C++." << endl;
-    cout << "They contain function declarations and macros." << endl;
-    cout << "For example, <iostream> is a header file that allows us to use input and output streams." << endl;
-    cout << "We can include header files using the #include directive." << endl;
+  
 
 
 
