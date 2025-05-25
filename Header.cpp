8e3f8353d08e3367b3f7b1d0259fe4b79d190273
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-25 23:23:16 +0530

    enter

--- Header.cpp
+++ Header.cpp
@@ -6,6 +6,7 @@ int main(){
     cout << "Header files are used to include libraries in C++." << endl;
     cout << "They contain function declarations and macros." << endl;
     cout << "For example, <iostream> is a header file that allows us to use input and output streams." << endl;
+    cout << "We can include header files using the #include directive." << endl;
 
 
 
