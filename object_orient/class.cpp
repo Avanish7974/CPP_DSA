70c99ca2f3c476a826f0e2ca4e4ffe53240f82de
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-27 19:31:57 +0530

    access

---
+++ object_orient/class.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class MyClass {
+
+};
+// the access specifier of the class is private by default, so the members of the class are not accessible outside the class.
+int main(){
+  
+
+
+
+    
+
+    return 0;
+}
