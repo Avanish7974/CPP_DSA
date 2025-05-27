bf7e0749c703eb38b4ab6a371f037609c37086e5
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-27 19:41:00 +0530

    object

--- object_orient/class.cpp
+++ object_orient/class.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
-class MyClass {
+class operation{
+public: // Public access specifier allows members to be accessible outside the class    
+    int n1,n2;
 
 };
 // the access specifier of the class is private by default, so the members of the class are not accessible outside the class.
 int main(){
+    operation op; // Creating an object of the class operation
+    cout<<op.n1<<endl; // Trying to access the member n1 of the class operation
+    cout<<op.n2<<endl; // Trying to access the member n2 of the class operation
+    // Since n1 and n2 are not initialized, they will contain garbage values.
+    // This will print some random values.
+
   
 
 
---
+++ object_orient/object.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class MyClass {
+    public: // Public access specifier allows members to be accessible outside the class
+        int n1, n2; // Member variables
+};
+int main(){
+    MyClass obj; // Creating an object of MyClass
+    obj.n1 = 10; // Initializing member variable n1
+    obj.n2 = 20; // Initializing member variable n2
+    cout << "n1: " << obj.n1 << endl; // Accessing and printing member variable n1
+    cout << "n2: " << obj.n2 << endl; // Accessing and printing member variable n2
+  
+    return 0;
+}
