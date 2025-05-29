3f3ad281e154dec35d2c4b97117defbedc82fe78
Avanish7974 <avanishsingh4318@gmail.com>
2025-05-29 20:01:03 +0530

    commit

--- specifier/modifier.cpp
+++ specifier/modifier.cpp
@@ -1,1 +1,3 @@
-// if we not use any access specifier, the default access specifier is private  
+// if we not use any access specifier, the default access specifier is private 
+// in the case of member function we can not use private access specifier and protected access specifier 
+// we only use public access specifier for member function because we want to access them outside the class
