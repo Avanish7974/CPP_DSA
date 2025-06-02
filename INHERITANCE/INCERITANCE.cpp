4c79da32c5f62eedfc162bd736a059387e4bf7cc
Avanish7974 <avanishsingh4318@gmail.com>
2025-06-02 19:56:04 +0530

    single inheritance

---
+++ INHERITANCE/INCERITANCE.cpp
@@ -0,0 +1,99 @@
+// // Explain Inheritance in C++:
+// // inheritance in c++ is a mechanism in which one class acquires the properties and behaviors of another class. 
+// // The class that acquires these properties and behaviors is called the derived class (child class) and the class from
+
+// // types of inheritance:
+// 1. Single Inheritance: A derived class can inherit only one base class.
+// 2. Multiple Inheritance: A derived class can inherit from multiple base classes.
+// 3. Multilevel Inheritance: A derived class can inherit from a base class that in turn inherits from another base class.
+// 4. Hierarchical Inheritance: A derived class can inherit from more than one base class, but each base class can only inherit from one other base class.
+// 5. Hybrid Inheritance: A derived class can inherit from a base class that in turn inherits from another base class, and it can also inherit from multiple other base classes.
+
+
+// // Advantages of Inheritance:
+// 1. Code Reusability: Inheritance allows us to reuse the code and properties of the base class in the derived class.
+// 2. Encapsulation: Inheritance promotes encapsulation by hiding the implementation details of the base class and providing a controlled interface for accessing and modifying the derived class's data members.
+// 3. Modularity: Inheritance allows us to modularize the code by separating the related classes into different base classes and derived classes.
+// 4. Flexibility: Inheritance allows us to extend the functionality of the base class by adding new members and functions to the derived class.
+
+// // Syntax for Inheritance in C++:
+// // 1. Single Inheritance syntax:
+//    class DerivedClassName : public BaseClassName {
+//    // Member declarations and methods
+//    };
+//    // Example:
+//    class Student : public Person {
+//    public:
+//    int rollNo;
+//    };
+//    // 2. Multiple Inheritance syntax:
+//    class DerivedClassName : public BaseClassName1, public BaseClassName2 {
+//    // Member declarations and methods
+//    };
+//    // Example:
+//    class Employee : public EmployeeInfo, public Address {
+//    public:
+//    int employeeID;
+//    };
+//    // 3. Multilevel Inheritance syntax:
+//    class DerivedClassName : public BaseClassName1 {
+//    public:
+//    // Member declarations and methods
+//    };
+//    class BaseClassName1 : public BaseClassName2 {
+//    // Member declarations and methods
+//    };
+//    class BaseClassName2 {
+//    // Member declarations and methods
+//    };
+//    // Example:
+//    class Manager : public Employee, public Department {
+//    public:
+//    int managerID;
+//    };
+//    // 4. Hierarchical Inheritance syntax:
+//    class DerivedClassName : public BaseClassName1 {
+//    public:
+//    // Member declarations and methods
+//    };
+//    class BaseClassName1 {
+//    // Member declarations and methods
+//    };
+//    class BaseClassName2 {
+//    // Member declarations and methods
+//    };
+//    // Example:
+//    class Employee : public BaseClassName1 {
+//    public:
+//    int employeeID;
+//    };
+//    class Department : public BaseClassName2 {
+//    public:
+//    string departmentName;
+//    };
+//    // 5. Hybrid Inheritance syntax:
+//    class DerivedClassName : public BaseClassName1, public BaseClassName2 {
+//    public:
+//    // Member declarations and methods
+//    };
+//    class BaseClassName1 : public BaseClassName3 {
+//    // Member declarations and methods
+//    };
+//    class BaseClassName2 : public BaseClassName4 {
+//    // Member declarations and methods
+//    };
+//    class BaseClassName3 {
+//    // Member declarations and methods
+//    };
+//    class BaseClassName4 {
+//    // Member declarations and methods
+//    };
+//    // Example:
+//    class Employee : public EmployeeInfo, public Address {
+//    public:
+//    int employeeID;
+//    };
+//    class Manager : public Employee, public Department {
+//    public:
+//    int managerID;
+//    };
---
+++ INHERITANCE/Single_inhert.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+// program for single inheritance with constructor and destructor
+class RGPV{
+    public:
+    string uname;
+    int ucode;
+    RGPV(){
+        cout<<"\n RGPV Default Constructor Called ";
+    }
+    ~RGPV(){
+        cout<<"\n RGPV Destructor  Called ";
+    }
+    void getinfo(){
+        cout<<"\nEnter University Name : ";
+        cin>>uname;
+        cout<<"\nEnter University Code : ";
+        cin>>ucode;
+    };
+
+
+    
+
+};
+
+class Student: public RGPV{
+    public:
+    int roll;
+    string name;
+    void acceptdata(){
+        getinfo();
+        cout<<"\nEnter Student Name : ";
+        cin>>name;
+        cout<<"\nEnter Roll Number : ";
+        cin>>roll;
+    }
+    void display(){
+        cout<<"\nUniversity Name : "<<uname;
+        cout<<"\nUniversity Code : "<<ucode;
+        cout<<"\nStudent Name : "<<name;
+        cout<<"\nRoll Number : "<<roll;
+    }
+    Student(){
+        cout<<"\nStudent Default Constructor Called ";
+    }
+    ~Student(){
+        cout<<"\nStudent Destructor Called ";
+    }
+};    
+int main(){
+    Student s;
+    s.acceptdata();
+    s.display();
+    
+   
+    return 0;
+}
